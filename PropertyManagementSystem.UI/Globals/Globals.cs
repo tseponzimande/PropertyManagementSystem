cd907826d987540150945993c1f377927218f30c
Tshepo <Tshepon@blackmagic.co.za>
2026-04-25 16:13:54 +0200

    Setting up UI

--- PropertyManagementSystem.UI/Globals/Globals.cs
+++ PropertyManagementSystem.UI/Globals/Globals.cs
@@ -1,3 +1,5 @@
 ﻿global using PropertyManagementSystem.Application.Extensions;
 global using PropertyManagementSystem.Infrastructure.Extensions;
 global using PropertyManagementSystem.UI.Components;
+global using Radzen;
+global using Microsoft.AspNetCore.Components;
